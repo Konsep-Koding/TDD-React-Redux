82093e39cc518e68849da5bbaa9901dc5f84796d
zidniryi <zidni@halojasa.id>
2020-06-15 17:11:45 +0700

    Setup test file for redux

---
+++ src/actions/balance.test.js
@@ -0,0 +1,11 @@
+ import * as constants from './constants'
+ import  * as actions from './balance'
+
+
+ it('create an actions action balance', () => {
+     
+    const balance = 0
+    const expectedAction = {type: constants.SET_BALANCE, balance}
+
+    expect(actions.setBalance(balance)).toEqual(expectedAction)
+ });
