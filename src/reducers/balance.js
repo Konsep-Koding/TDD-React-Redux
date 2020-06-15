a8c90aa051df0740e4c104105c8ce28e4a88dcb0
zidniryi <zidni@halojasa.id>
2020-06-15 19:15:02 +0700

    Finish test and deposit test in redux nice

--- src/reducers/balance.js
+++ src/reducers/balance.js
@@ -4,9 +4,12 @@
       switch(action.type) {
            case constant.SET_BALANCE:
                return action.balance
+           case constant.DEPOSIT:
+               return state + action.deposit  
             default: 
                 return state
       }
  }
+
 
  export default balance
--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -6,6 +6,11 @@ import balanceReducer from './balance'
          const balance = 10
 
          expect(balanceReducer(undefined, {type: constant.SET_BALANCE, balance})).toEqual(balance)
+     });
 
+     it('should deposits into the balanced', () => {
+         const deposit = 10 
+         const initialState = 5
+         expect(balanceReducer(initialState, {type: constant.DEPOSIT, deposit})).toEqual(initialState + deposit)
      });
  });
