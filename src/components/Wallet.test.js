078c94b9fb17c10b249877303124156231079462
zidniryi <zidni@halojasa.id>
2020-06-15 18:03:16 +0700

    Configure connect wallet and test file

--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow, configure } from 'enzyme';
-import Wallet from './Wallet';
+import { Wallet } from './Wallet';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter(), disableLifecycleMethods: false });
