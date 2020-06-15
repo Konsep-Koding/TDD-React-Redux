615a21c2f115a995259a2744eb707f65aa166ac4
zidniryi <zidni@halojasa.id>
2020-06-15 16:58:49 +0700

    Initial project and library

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+       <h2>Loot Check</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import App from './App';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter(), disableLifecycleMethods: false });
+
+describe('App', () => {
+    const app = shallow(<App/>)
+    it('Render', () => {
+        expect(app).toMatchSnapshot();
+});
+});
