72ef948f3294998f44ae12df04d2c40509bbce94
zidniryi <zidni@halojasa.id>
2020-06-18 18:07:33 +0700

    Finish Asyc Test For Redux Thubk Action Middleware

---
+++ src/actions/bitcoin.js
@@ -0,0 +1,10 @@
+const { FETCH_BITCOIN } = require('./constants')
+
+export const fetchBitcoin = () => {
+     return dispatch => {
+         return fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+         .then(response => response.json())
+         .then(json => dispatch({type: FETCH_BITCOIN, bitcoin: json}))
+     }
+
+}
